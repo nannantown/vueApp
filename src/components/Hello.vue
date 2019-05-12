4c8e55ed2fb92b43f33ea23b8bbe28720637ac20
Koki Naniwa <namihna@icloud.com>
2019-05-12 13:53:58 +0900

    3
    3

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,4 +1,32 @@
 
 <template>
-
+	<view>
+		<text class="text-color-primary">{{message}}</text>
+		<button :title="btnMessage" :on-press="handleClick" />
+		<text>{{clickCount}}</text>
+	</view>
 </template>
+
+<script>
+	export default {
+		data (){
+			return {
+				message: 'Welcome to my application',
+				btnMessage: 'Click Me',
+				clickCount: 0
+			}
+		},
+		methods: {
+			handleClick () {
+				this.clickCount ++
+			}
+		}
+	}
+</script>
+
+<style>
+
+.text-color-primary {
+  color: blue;
+}
+</style>
